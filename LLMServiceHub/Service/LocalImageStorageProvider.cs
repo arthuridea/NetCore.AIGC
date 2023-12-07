467bb911beeab418f9013810fb85376bda1d86ae
arthuridea w <2337583+arthuridea@users.noreply.github.com>
2023-12-07 16:25:46 +0800

    feat: a demonstration of local image staorage provider.

--- LLMServiceHub/Service/LocalImageStorageProvider.cs
+++ LLMServiceHub/Service/LocalImageStorageProvider.cs
@@ -7,12 +7,19 @@
     public class LocalImageStorageProvider : IImageStorageProvider
     {
         private readonly ILogger<LocalImageStorageProvider> _logger;
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IWebHostEnvironment _env;
         /// <summary>
         /// Initializes a new instance of the <see cref="LocalImageStorageProvider"/> class.
         /// </summary>
-        public LocalImageStorageProvider(ILogger<LocalImageStorageProvider> logger)
+        public LocalImageStorageProvider(
+            IHttpClientFactory httpClientFactory,
+            IWebHostEnvironment env,
+            ILogger<LocalImageStorageProvider> logger)
         {
+            _httpClientFactory = httpClientFactory;
             _logger = logger;
+            _env = env;
         }
         /// <summary>
         /// Saves the specified URL.
@@ -22,8 +29,28 @@
         /// <returns></returns>
         public async Task<string> Save(string url, string pathname)
         {
+            string localPathName = Path.Combine(_env.WebRootPath, pathname);
+            var httpClient = _httpClientFactory.CreateClient();
+            var response = await httpClient.GetAsync(url);
+            if (response != null && response.IsSuccessStatusCode)
+            {
+                response.EnsureSuccessStatusCode();
+                using(var stream  = await response.Content.ReadAsStreamAsync())
+                {
+                    _logger.LogDebug($"LOCAL PATH -> {localPathName}");
+                    string directory = Path.GetDirectoryName(localPathName);
+                    if(!Directory.Exists(directory))
+                    {
+                        Directory.CreateDirectory(directory);
+                    }
+                    using (var fs = new FileStream(localPathName, FileMode.Create))
+                    {
+                        await stream.CopyToAsync(fs);
+                    }
+                }
+            }
             _logger.LogInformation($"[{nameof(LocalImageStorageProvider)}] save: {url} -> {pathname}");
-            return await Task.FromResult(url);
+            return await Task.FromResult(localPathName);
         }
     }
 }
